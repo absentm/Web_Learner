1d5d482d99a79992508ed2b3ec7726f5d06caab8
mduanfire <absentm@163.com>
2017-04-01 21:17:30 +0800

    home images show .

--- demos/bsp_demo/js/bsp_index.js
+++ demos/bsp_demo/js/bsp_index.js
@@ -2,6 +2,27 @@
  * Created by DUAN on 2017/4/1.
  */
 
+// var weatherUrl = "http://ip.taobao.com/service/getIpInfo.php?ip=222.140.98.151";
+// $(document).ready(function () {
+//     $.ajax({
+//         type: "GET",
+//         dataType: "jsonp",
+//         url: weatherUrl,
+//         async: false,
+//         jsonp: 'callback',
+//         success: function (jsonData) {
+//             console.log(jsonData);
+//         }
+//     });
+// });
+
+
+// $(document).ready(function () {
+//     $.getJSON(weatherUrl, function (jsonData) {
+//         console.log(jsonData);
+//     });
+// });
+
 
 $(function () {
     // $("#id_bsp_about").click(function () {
@@ -12,7 +33,6 @@ $(function () {
         alert("btn_clicked !!!");
     });
 });
-
 
 
 // $(function () {
