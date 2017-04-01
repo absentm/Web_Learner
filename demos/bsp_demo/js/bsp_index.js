c5685d57fecb6d0642abb3437279a477c30a95e3
mduanfire <absentm@163.com>
2017-04-01 11:23:54 +0800

    nav and carousel .

---
+++ demos/bsp_demo/js/bsp_index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by DUAN on 2017/4/1.
+ */
+
+
+$(function () {
+    // $("#id_bsp_about").click(function () {
+    //     alert("About !!!");
+    // });
+
+    $("#btn_home").click(function () {
+        alert("btn_clicked !!!");
+    });
+});
+
+
+
+// $(function () {
+//
+// });
